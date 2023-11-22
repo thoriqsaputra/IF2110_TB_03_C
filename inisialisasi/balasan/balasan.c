ed3f963de9ef7adce247171f4467e20098abc4e7
Ahmad Thoriq Saputra <saputrathoriq@gmail.com>
2023-11-22 17:55:56 +0700

    Balasan

--- inisialisasi/balasan/balasan.c
+++ inisialisasi/balasan/balasan.c
@@ -9,7 +9,7 @@ void CreateListBalasan(ListDinBalasan *l, int capacity)
 /* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity dan diisi KICAUAN */
 {
     NEFFBalasan(*l) = 0;
-    ContentBalasan(*l) = (Balasan *)malloc(capacity * sizeof(Balasan));
+    ContentBalasan(*l) = (BALASAN *)malloc(capacity * sizeof(BALASAN));
     CAPACITYBalasan(*l) = capacity;
 }
 
@@ -22,13 +22,101 @@ void dealocateBalasan(ListDinBalasan *l)
     CAPACITYBalasan(*l) = 0;
 }
 
-// void addBalasan(ListDinBalasan *l, Balasan B)
-// /* I.S. l berisi kicauan, mungkin penuh */
-// /* F.S. l diisi kicauan baru nEffBalasan bertambah 1, capacity balasan menyesuaikan*/
-// {
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int LengthListBalasan(ListDinBalasan l)
+/* Mengirimkan banyaknya elemen efektif list */
+/* Mengirimkan nol jika list l kosong */
+/* *** Daya tampung container *** */
+{
+    return NEFFBalasan(l);
+}
 
-// }
+/* ********** OPERASI LAIN ********** */
+void copyListDinBalasan(ListDinBalasan lIn, ListDinBalasan *lOut)
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */
+{
+    int i;
+    dealocateBalasan(lOut);
+    CreateListBalasan(lOut, CAPACITYBalasan(lIn));
+    NEFFBalasan(*lOut) = NEFFBalasan(lIn);
+    for (i = 0; i < NEFFBalasan(lIn); i++)
+    {
+        ELMTBalasan(*lOut, i) = ELMTBalasan(lIn, i);
+    }
+}
 
+BALASAN* getBalasan(ListDinListB *lb, int IDKicauan, int IDBalasan) {
+
+    ListDinBalasan* l = getListDinBalasan(lb, IDKicauan);
+
+    if (l == NULL || IDKicauB(*l) == INVALID_ID) {
+
+        return NULL;
+    }
+
+    for (int i = 0; i < NEFFBalasan(*l); i++) {
+        if (IDBalasan(ELMTBalasan(*l, i)) == IDBalasan) {
+            return &ELMTBalasan(*l, i);
+        }
+    }
+
+    // Return a special value to indicate not found
+    return NULL;
+}
+
+/* ********** BOOLEAN ********** */
+boolean isFullBalasan(ListDinBalasan l)
+/* return true jika penuh */
+{
+    return (NEFFBalasan(l) == CAPACITYBalasan(l));
+}
+
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void expandListDinBalasan(ListDinBalasan *l, int num)
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+{
+    ListDinBalasan temp;
+    CreateListBalasan(&temp, CAPACITYBalasan(*l) + num);
+    CAPACITYBalasan(*l) += num;
+    copyListDinBalasan(*l, &temp);
+    free(ContentBalasan(*l));
+    ContentBalasan(*l) = (BALASAN *)malloc(CAPACITYBalasan(temp) * sizeof(BALASAN));
+    copyListDinBalasan(temp, l);
+    dealocateBalasan(&temp);
+}
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListDinBalasan(ListDinBalasan *l, BALASAN val)
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yanDg baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+{
+    int nefplus = NEFFBalasan(*l)+1;
+
+    if (nefplus >= CAPACITYBalasan(*l)) {
+        expandListDinBalasan(l, 4);
+    }
+    ELMTBalasan(*l, LengthListBalasan(*l)) = val;
+    NEFFBalasan(*l) += 1;
+}
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastListDinBalasan(ListDinBalasan *l, BALASAN *val)
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+{
+    *val = ELMTBalasan(*l, LengthListBalasan(*l) - 1);
+    NEFFBalasan(*l) -= 1;
+}
 
 /* ********** KONSTRUKTOR ListDinBalasan ********** */
 /* Konstruktor : create list kosong  */
@@ -40,6 +128,7 @@ void CreateListDinB(ListDinListB *lb, int capacity)
     ContentListB(*lb) = (ListDinBalasan *)malloc(capacity * sizeof(ListDinBalasan));
     CAPACITYListB(*lb) = capacity;
 }
+
 void dealocateListDinB(ListDinListB *lb)
 /* I.S. l terdefinisi; */
 /* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
@@ -49,62 +138,207 @@ void dealocateListDinB(ListDinListB *lb)
     CAPACITYListB(*lb) = 0;
 }
 
-// void addBalasanKeListDin(ListDinListB *lb, Balasan B);
-// /* I.S. l berisi kicauan, mungkin penuh */
-// /* F.S. l diisi kicauan baru nEffBalasan bertambah 1, capacity balasan menyesuaikan*/
-
-void loadBalasanConfig(char filename[], ListDinListB *lb)
-/* I.S. l kosong; */
-/* F.S. l diisi sesuai config */
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int LengthListB(ListDinListB l)
+/* Mengirimkan banyaknya elemen efektif list */
+/* Mengirimkan nol jika list l kosong */
+/* *** Daya tampung container *** */
 {
-    // Kamus Lokal
-    int i, j, CapListB, CapB, IDKicauan;
+    return NEFFListB(l);
+}
 
-
-    // ALGORITMA
-    STARTWORDFILE(filename);
-    CapListB = wordToInt(currentWord);
-    CreateListDinB(lb, CapListB);
-
-    for ( i = 0; i < CAPACITYListB(*lb); i++) {
-        ListDinBalasan l;
-        ADVWORDFILE();
-        IDKicauan = wordToInt(currentWord);
-        ADVWORDFILE();
-        CapB = wordToInt(currentWord);
-        CreateListBalasan(&l, CapB);
-        IDKicauB(l) = IDKicauan;
-
-        for (j = 0; j < CAPACITYBalasan(l); j++) {
-            ADVWORDFILE();
-            IDBalasan(ELMTBalasan(l, j)) = currentWord; //baru dalam Word, perlu dipisah kedua IDs -1 1
-            ADVWORDFILE();
-            TxtBalasan(ELMTBalasan(l, j)) = currentWord;
-            ADVWORDFILE();
-            AuthorBalasan(ELMTBalasan(l, j)) = currentWord;
-            ADVWORDFILE();
-            DatetimeBalasan(ELMTBalasan(l, j)) = currentWord;
-        }
-        NEFFBalasan(l) = CAPACITYBalasan(l);
-        ELMTListB(*lb, i) = l;
+/* ********** OPERASI LAIN ********** */
+void copyListDinB(ListDinListB lIn, ListDinListB *lOut)
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */
+{
+    int i;
+    dealocateListDinB(lOut);
+    CreateListDinB(lOut, CAPACITYListB(lIn));
+    NEFFListB(*lOut) = NEFFListB(lIn);
+    for (i = 0; i < NEFFListB(lIn); i++)
+    {
+        ELMTListB(*lOut, i) = ELMTListB(lIn, i);
     }
-    NEFFListB(*lb) = CAPACITYListB(*lb);
+}
 
+ListDinBalasan* getListDinBalasan(ListDinListB *lb, int IDKicauan) {
+    
+    for (int i = 0; i < NEFFListB(*lb); i++) {
+        if (IDKicauB(ELMTListB(*lb, i)) == IDKicauan) {
+            return &ELMTListB(*lb, i);
+        }
+    }
+
+    // Return a special value to indicate not found
+    return NULL;
 }
+
 /* ********** BOOLEAN ********** */
-boolean isFullBalasan(ListDinBalasan l)
-/* return true jika penuh */
-{
-    return (NEFFBalasan(l) == CAPACITYBalasan(l));
-}
 boolean isFullListDinB(ListDinListB lb)
 /* return true jika penuh */
 {
     return (NEFFListB(lb) == CAPACITYListB(lb));
 }
 
-/* ********** FUNGSI LAIN ********** */
-void displayBalasan(ListDinBalasan B)
+boolean isKicauanInList(ListDinListB lb, int IDKicauan) {
+
+    for(int i = 0; i < NEFFListB(lb); i++) {
+        if(IDKicauB(ELMTListB(lb, i)) == IDKicauan) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void expandListDinB(ListDinListB *l, int num)
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+{
+    ListDinListB temp;
+    CreateListDinB(&temp, CAPACITYListB(*l) + num);
+    CAPACITYListB(*l) += num;
+    copyListDinB(*l, &temp);
+    free(ContentListB(*l));
+    ContentListB(*l) = (ListDinBalasan *)malloc(CAPACITYListB(temp) * sizeof(ListDinBalasan));
+    copyListDinB(temp, l);
+    dealocateListDinB(&temp);
+}
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListDinB(ListDinListB *l, ListDinBalasan val)
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yanDg baru */
+{
+    int nefplus = NEFFListB(*l)+1;
+
+    if ( nefplus >= CAPACITYListB(*l)) {
+        expandListDinB(l, 4);
+    }
+    
+    ELMTListB(*l, LengthListB(*l)) = val;
+    NEFFListB(*l) += 1;
+}
+
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastListDinB(ListDinListB *l, ListDinBalasan *val)
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+{
+    *val = ELMTListB(*l, LengthListB(*l) - 1);
+    NEFFListB(*l) -= 1;
+}
+
+
+/* ********* CONFIGS ********* */
+void loadBalasanConfig(char filename[], ListDinListB *lb, ListTree *LT)
+/* I.S. l kosong; */
+/* F.S. l diisi sesuai config */
+{
+    // Kamus Lokal
+    int i, j, CapListB, CapB, IDKicauan;
+
+    // ALGORITMA
+    STARTWORDFILE(filename);
+    // Mendapatkan jumlah kicaun yang memiliki balasan
+    CapListB = wordToInt(currentWord);
+    // Membuat list dengan banyaknya kicauan
+    CreateListDinB(lb, CapListB);
+
+    for ( i = 0; i < CAPACITYListB(*lb); i++) {
+        ListDinBalasan l;
+        ADVWORDFILE();
+        // Mendapatkan IDKicauan
+        IDKicauan = wordToInt(currentWord);
+        ADVWORDFILE();
+        // Mendapatkan Jumlah balasan pada IDKicauan
+        CapB = wordToInt(currentWord);
+        // Membuat list dengan banyaknya balasan
+        CreateListBalasan(&l, CapB);
+        // Menyimpan IDKicauan
+        IDKicauB(l) = IDKicauan;
+        // Membuat tree baru dan IDKicauan sebagai root
+        TreeNode* parent = createNode(IDKicauan);
+
+        // Dalam looping jumlah balasan membaca informasi terkait balasan tersebut
+        for (j = 0; j < CAPACITYBalasan(l); j++) {
+            ADVWORDFILE();
+            // Membaca ID yang dibalas ID balasan
+            Word ID = currentWord;
+            int currentResult = 0;
+            boolean isNegative = false;  
+            for (int i = 0; i < ID.Length; i++) {
+                if (ID.TabWord[i] == '-') {
+                    isNegative = true;  
+                } else if (ID.TabWord[i] == ' ') {
+                    if (isNegative) {
+                        currentResult = -currentResult;  
+                        isNegative = false;
+                    }
+                    // Menyimpan ID yang dibalas
+                    idDiBalas(ELMTBalasan(l, j)) = currentResult;
+                    currentResult = 0;
+                } else {
+                    currentResult = currentResult * 10 + (ID.TabWord[i] - '0');
+                }
+            }
+            if (isNegative) {
+                currentResult = -currentResult;  
+            }
+            // Menyimpan ID Balasan
+            IDBalasan(ELMTBalasan(l, j)) = currentResult;
+
+            // Membuat node baru sebagai anak dari root ID Kicauan
+            TreeNode* Child = createNode(IDBalasan(ELMTBalasan(l,j)));
+            
+            // Proses pemasukan ke tree dengan menggunakan IDBalasan/IDKicauan
+            if (idDiBalas(ELMTBalasan(l, j)) == -1) {
+                addChild(parent, Child);
+            } else {
+                addChild(SearchTree(parent, idDiBalas(ELMTBalasan(l, j))), Child);
+            }
+
+            // Lanjut ke txt balasan dst
+            ADVWORDFILE();
+            TxtBalasan(ELMTBalasan(l, j)) = currentWord;
+            ADVWORDFILE();
+            AuthorBalasan(ELMTBalasan(l, j)) = currentWord;
+            ADVWORDFILE();
+            // Membaca DateTime lalu mengubanya ke typdef struct DATETIME
+            DATETIME t;
+            Word temp = currentWord;
+            int DD = (int)(temp.TabWord[0] % 48) * 10 + (int)(temp.TabWord[1] % 48);
+            int MM = (int)(temp.TabWord[3] % 48) * 10 + (int)(temp.TabWord[4] % 48);
+            int YY = (int)(temp.TabWord[6] % 48) * 1000 + (int)(temp.TabWord[7] % 48) * 100 + (int)(temp.TabWord[8] % 48) * 10 + (int)(temp.TabWord[9] % 48);
+            int hh = (int)(temp.TabWord[11] % 48) * 10 + (int)(temp.TabWord[12] % 48);
+            int mm = (int)(temp.TabWord[14] % 48) * 10 + (int)(temp.TabWord[15] % 48);
+            int ss = (int)(temp.TabWord[17] % 48) * 10 + (int)(temp.TabWord[18] % 48);
+            CreateDATETIME(&t, DD, MM, YY, hh, mm, ss);
+            DatetimeBalasan(ELMTBalasan(l, j)) = t;
+
+            }
+        // Menambahkan tree IDKicauan pada akhir list tree
+        insertLastListTree(LT, parent);
+        // NEFF jadi jumlah balasan
+        NEFFBalasan(l) = CAPACITYBalasan(l);
+        // Menyimpang list balasan pada ListB
+        ELMTListB(*lb, i) = l;
+
+    }
+    // Neff menjadi banyaknya Kicauan yang memiliki balasan
+    NEFFListB(*lb) = CAPACITYListB(*lb);
+}
+
+void displayConfigBalasan(ListDinBalasan B)
 {
     // KAMUS LOKAL
     int i;
@@ -113,13 +347,12 @@ void displayBalasan(ListDinBalasan B)
     printf("%d\n",IDKicauB(B));
     printf("%d\n",CAPACITYBalasan(B));
     for (i = 0; i < CAPACITYBalasan(B); i++) {
-        printWord(IDBalasan(ELMTBalasan(B, i)));
-        printf("\n");
+        printf("%d %d\n", idDiBalas(ELMTBalasan(B,i)), IDBalasan(ELMTBalasan(B,i)));
         printWord(TxtBalasan(ELMTBalasan(B, i)));
         printf("\n");
         printWord(AuthorBalasan(ELMTBalasan(B, i)));
         printf("\n");
-        printWord(DatetimeBalasan(ELMTBalasan(B, i)));
+        TulisDATETIME(DatetimeBalasan(ELMTBalasan(B, i)));
         printf("\n");
     }
 }
@@ -133,11 +366,8 @@ void displayListB(ListDinListB lb)
     printf("%d\n", CAPACITYListB(lb));
     for (i = 0; i < CAPACITYListB(lb); i++)
     {
-        displayBalasan(ELMTListB(lb, i));
-        printf("\n");
+        displayConfigBalasan(ELMTListB(lb, i));
     }
-
-
 }
 
 int wordToInt(Word kata)
@@ -152,3 +382,4 @@ int wordToInt(Word kata)
     }
     return res;
 }
+
--- inisialisasi/balasan/balasan.h
+++ inisialisasi/balasan/balasan.h
@@ -2,37 +2,44 @@
 #define BALASAN_H
 
 #include "../../ADT/wordmachine.h"
+#include "../../ADT/datetime.h"
+#include "../../ADT/tree.h"
+#include "../kicauan/kicauan.h"
+
+
 
 /*  Kamus Umum */
 #define IDX_MIN 0
 /* Indeks minimum list */
 #define IDX_UNDEF -1
 /* Indeks tak terdefinisi*/
+#define INVALID_ID -999
+
 
 /* Definisi elemen dan koleksi objek */
-typedef struct
-{
-    Word IDBalasan;
+typedef struct BALASAN {
+    int idDiBalas;
+    int IDBalasan;
     Word TextBalasan;
     Word Author;
-    Word DT;
-} Balasan;
+    DATETIME DT;
+} BALASAN;
 
 /* ********** SELEKTOR BALASAN ********** */
+#define idDiBalas(l) (l).idDiBalas
 #define IDBalasan(l) (l).IDBalasan
 #define TxtBalasan(l) (l).TextBalasan
 #define AuthorBalasan(l) (l).Author
 #define DatetimeBalasan(l) (l).DT
 
-typedef struct 
-{
+typedef struct ListDinBalasan {
     int IDKicau;
-    Balasan *contentBalasan;
+    BALASAN *contentBalasan;
     int capacitybalasan;
     int nEffBalasan;
 } ListDinBalasan;
 
-/* ********** SELEKTOR List Dinamis Balasan ********** */
+/* ********** SELEKTOR List Dinamis BALASAN ********** */
 #define IDKicauB(l) (l).IDKicau
 #define ELMTBalasan(l, i) (l).contentBalasan[i]
 #define ContentBalasan(l) (l).contentBalasan
@@ -55,44 +62,112 @@ typedef struct {
 /* Konstruktor : create list kosong  */
 void CreateListBalasan(ListDinBalasan *l, int capacity);
 /* I.S. l sembarang, capacity > 0 */
-/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity dan diisi KICAUAN */
+/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity dan diisi BALASAN */
 
 void dealocateBalasan(ListDinBalasan *l);
 /* I.S. l terdefinisi; */
 /* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
 
-// void addBalasan(ListDinBalasan *l, Balasan B);
-// /* I.S. l berisi kicauan, mungkin penuh */
-// /* F.S. l diisi kicauan baru nEffBalasan bertambah 1, capacity balasan menyesuaikan*/
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int LengthListBalasan(ListDinBalasan l);
+/* Mengirimkan banyaknya elemen efektif list */
+/* Mengirimkan nol jika list l kosong */
+/* *** Daya tampung container *** */
 
-/* ********** KONSTRUKTOR ListDinBalasan ********** */
-/* Konstruktor : create list kosong  */
-void CreateListDinB(ListDinListB *lb, int capacity);
-/* I.S. l sembarang, capacity > 0 */
-/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity dan diisi KICAUAN */
+/* ********** OPERASI LAIN ********** */
+void copyListDinBalasan(ListDinBalasan lIn, ListDinBalasan *lOut);
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */
 
-void dealocateListDinB(ListDinListB *lb);
-/* I.S. l terdefinisi; */
-/* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
+BALASAN* getBalasan(ListDinListB *lb, int IDKicauan, int IDBalasan);
 
-// void addBalasanKeListDin(ListDinListB *lb, Balasan B);
-// /* I.S. l berisi kicauan, mungkin penuh */
-// /* F.S. l diisi kicauan baru nEffBalasan bertambah 1, capacity balasan menyesuaikan*/
-
-void loadBalasanConfig(char filename[], ListDinListB *lb);
-/* I.S. l kosong; */
-/* F.S. l diisi sesuai config */
+void printBalasan(BALASAN B);
 
 /* ********** BOOLEAN ********** */
 boolean isFullBalasan(ListDinBalasan l);
 /* return true jika penuh */
 
-boolean isFullListDinB(ListDinListB l);
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void expandListDinBalasan(ListDinBalasan *l, int num);
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListDinBalasan(ListDinBalasan *l, BALASAN val);
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yanDg baru */
+
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastListDinBalasan(ListDinBalasan *l, BALASAN *val);
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+/* ********** KONSTRUKTOR ListDinBalasan ********** */
+/* Konstruktor : create list kosong  */
+void CreateListDinB(ListDinListB *lb, int capacity);
+/* I.S. l sembarang, capacity > 0 */
+/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity dan diisi ListBalasan */
+
+void dealocateListDinB(ListDinListB *lb);
+/* I.S. l terdefinisi; */
+/* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int LengthListB(ListDinListB l);
+/* Mengirimkan banyaknya elemen efektif list */
+/* Mengirimkan nol jika list l kosong */
+/* *** Daya tampung container *** */
+
+/* ********** OPERASI LAIN ********** */
+void copyListDinB(ListDinListB lIn, ListDinListB *lOut);
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */
+
+ListDinBalasan* getListDinBalasan(ListDinListB *lb, int IDKicauan);
+
+/* ********** BOOLEAN ********** */
+boolean isFullListDinB(ListDinListB lb);
 /* return true jika penuh */
 
-/* ********** FUNGSI LAIN ********** */
-// void printWord(Word kata);
+boolean isKicauanInList(ListDinListB lb, int IDKicauan);
 
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void expandListDinB(ListDinListB *l, int num);
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListDinB(ListDinListB *l, ListDinBalasan val);
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yanDg baru */
+
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastListDinB(ListDinListB *l, ListDinBalasan *val);
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+/* ********* CONFIGS ********* */
+void loadBalasanConfig(char filename[], ListDinListB *lb, ListTree *LT);
+/* I.S. l kosong; */
+/* F.S. l diisi sesuai config */
+
+/* ********** FUNGSI LAIN ********** */
 void displayBalasan(ListDinBalasan B);
 
 void displayListB(ListDinListB lb);

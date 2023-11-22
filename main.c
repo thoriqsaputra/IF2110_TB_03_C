5d10c45bb334852061926e90cf4978c347e77b96
satriadhikara <satriadhikara@gmail.com>
2023-11-22 19:26:07 +0700

    refactor: main sementara

--- main.c
+++ main.c
@@ -1,46 +1,105 @@
 #include "ADT/tempRun.h"
 #include "Lib/commands.h"
 #include <stdio.h>
-#include "inisialisasi/pengguna/pengguna.c"
+#include <stdlib.h>
+// #include "Lib/ganti_profil/gantiprofil.h"
+#include "inisialisasi/pengguna/pengguna.h"
+#include "inisialisasi/draf/draf.h"
+#include "fitur/drafKicauan/drafKicauan.h"
 
+// main ini masi sementara
+// yang baru bisa
+// - Profil
+// - Pengguna
+// - Draf Kicauan
 
-boolean isWordEqual(Word input, Word cek)
-{
-
-    if (input.Length != cek.Length)
-    {
-        return false;
-    }
-    else
-    {
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (input.TabWord[i] != cek.TabWord[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// command run sementara
+// gcc -o main main.c inisialisasi/draf/draf.c inisialisasi/pengguna/pengguna.c fitur/drafKicauan/drafKicauan.c
 
 int main()
 {
-    //Inisialiasi
+    printf(".______    __    __  .______      .______    __  .______      \n");
+    printf("|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\     \n");
+    printf("|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  |    \n");
+    printf("|   _  <  |  |  |  | |      /     |   _  <  |  | |      /     \n");
+    printf("|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\  \\----.\n");
+    printf("|______/   \\______/  | _| `._____||______/  |__| | _| `._____| \n");
+
+    printf("\nSelamat datang di BurBir.\n\n");
+
+    printf("Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan menggunakan metode (pengambilan data berupa) Focused Group Discussion kedua di zamannya.\n\n");
+
+    // printf("Silahkan masukan folder konfigurasi untuk dimuat: ");
+
+    // STARTWORDINPUT();
+
+    // Word folder = currentWord;
+
+    boolean isLogged = false; // sementara true
+
+    // Inisialiasi (sementara)
     ListUserStatik LU;
     CreateEmptyPengguna(&LU);
-    loadPenggunaConfig("configs/config-1/pengguna.config",&LU);
+    loadPenggunaConfig("configs/config-1/pengguna.config", &LU);
 
+    ListDinDraf LD;
+    loadDrafConfig("configs/config-1/draf.config", &LD);
+
+    currentUser CU;
+    CreateEmptyCurrentUser(&CU);
+
+    DrafKicauan DK;
+    CreateDrafKicauan(&DK);
+
+    // Nanti pake ini
+    // muat(Word folder, ListUserStatik * LU, ListDinBalasan * LB, ListDinDraf * LD, ListDinKicauan * LK)
+
+    printf("File konfigurasi berhasil dimuat! Selamat berkicau!\n\n");
 
     boolean runProgram = true;
-
     while (runProgram)
     {
         printf(">> ");
-        STARTWORD();
+
+        STARTWORD(); // gapake STARTWORDINPUT() aja?
         // command.tabword = currentWord.tabword  & command.length = currentWord.length
         Word command = currentWord;
+        Word lu = {"lu", 2};
 
+        while ((!isLogged))
+        {
+            if (isWordEqual(command, masukCmd))
+            {
+                Masuk(&LU, &CU, &isLogged);
+                loadDrafKicauan(LD, &DK, CU); // Load draf dari raw data sesuai dengan current user
+            }
+            else if (isWordEqual(command, daftarCmd))
+            {
+                Daftar(&LU);
+            }
+            else if (isWordEqual(command, tutupProgramCmd))
+            {
+                isLogged = true; // biar keluar loop
+                command = currentWord;
+                break;
+            }
+            else if (isWordEqual(command, lu))
+            {
+                for (int i = 0; i < LU.capacity; i++)
+                {
+                    tulisDataPengguna(&LU.buffer[i]);
+                }
+            }
+            else
+            {
+                printf("Anda Belum Login, silahkan MASUK terlebih dahulu!\n");
+            }
+            printf(">> ");
+            STARTWORD();
+            command = currentWord;
+        }
+
+        // Udah login
         if (isWordEqual(command, tutupProgramCmd))
         {
             fclose(pita);
@@ -58,6 +117,45 @@ int main()
                 printf("%c", kicau.TabWord[i]);
             }
             printf("\n");
+        }
+        else if (isWordEqual(command, gantiProfilCmd))
+        {
+            gantiProfil(&LU, &CU);
+        }
+        // Buat Debugging
+        else if (isWordEqual(command, currentU))
+        {
+            tulisDataPengguna(&LU.buffer[idUser(CU)]);
+        }
+        else if (isWordEqual(command, lihatProfilCmd))
+        {
+            STARTWORDINPUT();
+            Word findusername = currentWord;
+            lihatUser(&LU, findusername);
+        }
+        else if (isWordEqual(command, keluarCmd))
+        {
+            Keluar(&LU, &CU, &isLogged);
+        }
+        else if (isWordEqual(command, aturJenisAkunCmd))
+        {
+            aturJenisAkun(&LU, &CU);
+        }
+        else if (isWordEqual(command, ubahFotoProfilCmd))
+        {
+            ubahFotoProfil(&LU, &CU);
+        }
+        else if (isWordEqual(command, buatDrafCmd))
+        {
+            buatDraf(&DK, CU, &LD);
+        }
+        else if (isWordEqual(command, lihatDrafCmd))
+        {
+            lihatDraf(&DK, &LD, CU);
+        }
+        else
+        {
+            printf("Tidak ada command itu\n\n");
         }
     }
     return 0;
